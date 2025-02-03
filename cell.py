e851133e698588f838db182eaaa68f27d94e0cb3
Lesmian <lesmian3@gmail.com>
2025-02-03 21:57:38 +0100

    add cell draw move method

--- cell.py
+++ cell.py
@@ -11,6 +11,9 @@ class Cell:
         self._win = window
 
     def draw(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
         if self.has_left_wall:
             self._win.draw_line(Line(Point(point1.x, point1.y), Point(point1.x, point2.y)), "black")
         if self.has_right_wall:
@@ -19,4 +22,14 @@ class Cell:
             self._win.draw_line(Line(Point(point1.x, point1.y), Point(point2.x, point1.y)), "black")
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(point1.x, point2.y), Point(point2.x, point2.y)), "black")
+
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            fill_color = "gray"
+        else:
+            fill_color = "red"
+        
+        cell1_center = Point((self.point1.x + self.point2.x) / 2, (self.point1.y + self.point2.y) / 2)
+        cell2_center = Point((to_cell.point1.x + to_cell.point2.x) / 2, (to_cell.point1.y + to_cell.point2.y) / 2)
+        self._win.draw_line(Line(cell1_center, cell2_center), fill_color)
     
--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from window import Window
 
 def main():
     win = Window(800, 600)
-    cell = Cell(win)
-    cell.draw(Point(50, 100), Point(200, 250))
+    cell1 = Cell(win)
+    cell1.has_right_wall = False
+    cell1.draw(Point(50, 100), Point(200, 250))
 
-    cell = Cell(win)
-    cell.has_top_wall = False
-    cell.draw(Point(250, 100), Point(400, 250))
+    cell2 = Cell(win)
+    cell2.has_left_wall = False
+    cell2.draw(Point(250, 100), Point(400, 250))
+
+    cell1.draw_move(cell2, True)
 
     win.wait_for_close()
 
