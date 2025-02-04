e1e5c5fec375b25bef1387f7134b5e07d17e06b6
Lesmian <lesmian3@gmail.com>
2025-02-04 22:17:58 +0100

    add maze

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 
 
 from cell import Cell
+from maze import Maze
 from point import Point
 from window import Window
 
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell1.has_right_wall = False
-    cell1.draw(Point(50, 100), Point(200, 250))
-
-    cell2 = Cell(win)
-    cell2.has_left_wall = False
-    cell2.draw(Point(250, 100), Point(400, 250))
-
-    cell1.draw_move(cell2, True)
+    maze = Maze(10, 10, 4, 5, 50, 60, win)
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,49 @@
+import time
+from cell import Cell
+from point import Point
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.start_point = Point(x1, y1)
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            self._cells.append([])
+            for j in range(self.num_rows):
+                self._cells[i].append(Cell(self.win))
+                self._draw_cell(i, j)
+    
+    def _draw_cell(self, i, j):
+        cell = self._cells[i][j]
+        cell_start_point = Point(
+            self.start_point.x + i * self.cell_size_x, 
+            self.start_point.y + j * self.cell_size_y
+        )
+        cell_end_point = Point(
+            self.start_point.x + (i + 1) * self.cell_size_x, 
+            self.start_point.y + (j + 1) * self.cell_size_y
+        )
+        cell.draw(cell_start_point, cell_end_point)
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+    
